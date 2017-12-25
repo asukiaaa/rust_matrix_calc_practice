d6e066b57866a4116133267aa991e449d5d15bca
asuki <asukiaaa@gmail.com>
2017-12-25 22:25:40 +0900

    Print array and matrix of ndarray

---
+++ setup_memo.md
@@ -0,0 +1,21 @@
+
+Create project
+```
+cargo new rust_matrix_calc_practice --bin
+```
+
+Install library
+Add following line to `Cargo.toml`.
+```
+ndarray = "0.10.13"
+```
+
+Build
+```
+cargo build
+```
+
+Run
+```
+cargo run
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+extern crate ndarray;
+use ndarray::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let a = Array::<f64, _>::zeros(3);
+    println!("{:?}", a);
+    let b = Array::range(0., 9., 1.).into_shape((3, 3)).unwrap();
+    println!("{:?}", b);
 }
